21fca198096a43c10906366cbd43e90980d6cf83
Cosmin Vasilescu <c.a.vasilescu@student.tudelft.nl>
2026-02-23 18:06:58 +0100

    20260223_180658

--- run_delftblue.sh
+++ run_delftblue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=CAV-MIA-AISE-research
-#SBATCH --partition=gpu-a100-small
+#SBATCH --partition=gpu-a100
 #SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
